f4b56427347c055fcf7bc87b7f1f03762a6f13b0
Muhammad Ahsan <180093008+Muhammad-Farhan1@users.noreply.github.com>
2025-07-26 16:02:27 +0500

    feat: add visualization on the dataset of Sales Product

--- Sale Data/sale.py
+++ Sale Data/sale.py
@@ -28,7 +28,6 @@ print(f"\nNumber of Duplicates: {df.duplicated().sum()}")
 # ============================================
 print(f"\n✅ Total Sales: {df['Sales_Amount'].sum():.2f} $")
 
-# Missing already handled above
 Q1 = df['Sales_Amount'].quantile(0.25)
 Q3 = df['Sales_Amount'].quantile(0.75)
 IQR = Q3 - Q1
@@ -75,27 +74,63 @@ print("\nProduct Performance by Month:\n", performance_monthly.head())
 print("\nProduct Performance by Week:\n", performance_weekly.head())
 
 # ============================================
-# ✅ Task 6: Demographics ( Which gender brings in more revenue? which age group buys more? Which 5 cities perform best?)
+# ✅ Task 6: Demographics (Gender, Age Group, Cities)
 # ============================================
 revenue_by_gender = df.groupby('Customer_Gender')['Sales_Amount'].sum().sort_values(ascending=False)
 print("\nRevenue by Gender:\n", revenue_by_gender)
 
-# Age Group Buckets
 bins = [0, 18, 25, 35, 50, 65, 100]
 labels = ['Teen', 'Youth', 'Young Adult', 'Adult', 'Senior', 'Elder']
 df['Age_Group'] = pd.cut(df['Customer_Age'], bins=bins, labels=labels)
 
-revenue_by_age = df.groupby('Age_Group')['Sales_Amount'].sum().sort_values(ascending=False)
+revenue_by_age = df.groupby('Age_Group', observed=True)['Sales_Amount'].sum().sort_values(ascending=False)
 print("\nRevenue by Age Group:\n", revenue_by_age)
 
 top_cities = df.groupby('Sales_Region')['Sales_Amount'].sum().sort_values(ascending=False)
 print("\nTop 5 Sales Regions:\n", top_cities.head())
 
 # ============================================
-# ✅ Task 7: Customer Behavior(Most Frequently Purchased Products, Average Purchase Amount Per Order)
+# ✅ Task 7: Customer Behavior
 # ============================================
 most_sold_item = df['Product_Category'].value_counts().sort_values(ascending=False)
 average_purchase = df['Sales_Amount'].mean()
 
 print("\nMost Frequently Purchased Products:\n", most_sold_item)
 print(f"\nAverage Purchase Amount per Order: {average_purchase:.2f} $")
+
+# ================================================
+# ✅ Task 8:  Monthly Revenue Trend (Visualization)
+# ================================================
+plt.figure(figsize=(10, 5))
+plt.plot(monthly_sales.index.astype(str), monthly_sales.values , marker='o' , color='blue' , label='Monthly Revenue')
+plt.title("Total Revenue month")
+plt.xlabel("Months")
+plt.ylabel("Total Revenue($)")
+plt.legend(loc='upper right')
+plt.grid(True)
+plt.tight_layout()
+# plt.savefig("Total_Revenue_By_Monthly.png", dpi=300 , bbox_inches='tight')
+
+# ==============================================
+# ✅ Task 9: Top 5 Product Categories by Revenue
+# ==============================================
+top5_products = most_revenue.head(5)
+
+plt.figure(figsize=(8, 5))
+plt.bar(top5_products.index, top5_products.values, color='blue', edgecolor='white')
+plt.title("Top 5 Product Categories by Revenue")
+plt.xlabel("Product Category")
+plt.ylabel("Total Revenue ($)")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# ==============================================
+# ✅ Task 10: Revenue Distribution by Customer Age Group
+# ==============================================
+plt.figure(figsize=(7, 7))
+plt.pie(revenue_by_age.values, labels=revenue_by_age.index, autopct='%1.1f%%', startangle=140)
+plt.title("Revenue Distribution by Customer Age Group")
+plt.axis('equal')
+plt.tight_layout()
+plt.show()
